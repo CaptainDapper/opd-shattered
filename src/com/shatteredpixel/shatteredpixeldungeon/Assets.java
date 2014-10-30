0a4a5206efa8c0f996e832cf5e5a1948c7f05af2
sloanr333 <sloan@lanimals.com>
2014-10-30 02:56:34 -0700

    0.3.2bb

--- src/com/shatteredpixel/shatteredpixeldungeon/Assets.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/Assets.java
@@ -20,19 +20,16 @@ package com.shatteredpixel.shatteredpixeldungeon;
 import com.opd.openpixeldungeon.DefaultAssets;
 
 public class Assets extends DefaultAssets {
-
-	public static final String BANNERS	= "shattered/banners.png";
 	public static final String BRUTE	= "shattered/brute.png";
 	public static final String BUFFS_SMALL	= "shattered/buffs.png";
 	public static final String CRAB		= "shattered/crab.png";
 	public static final String FIREBALL		= "shattered/fireball.png";
-	
 	public static final String GNOLL	= "shattered/gnoll.png";
 	public static final String GOO		= "shattered/goo.png";
 	public static final String ICONS	= "shattered/icons.png";
 	public static final String ITEMS	= "shattered/items.png";
 	public static final String BUFFS_LARGE	= "shattered/large_buffs.png";
-	
 	public static final String PLANTS	= "shattered/plants.png";
+	
 	public static final String RAT		= "shattered/rat.png";
 }
