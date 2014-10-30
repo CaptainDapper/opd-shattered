a3d38a68c35af04d297e5c9f04af275447ea774f
sloanr333 <sloan@lanimals.com>
2014-10-30 02:44:58 -0700

    0.3.2b

--- src/com/shatteredpixel/shatteredpixeldungeon/Assets.java
+++ src/com/shatteredpixel/shatteredpixeldungeon/Assets.java
@@ -17,143 +17,22 @@
  */
 package com.shatteredpixel.shatteredpixeldungeon;
 
-public class Assets {
+import com.opd.openpixeldungeon.DefaultAssets;
 
-	public static final String ARCS_BG		= "shattered/arcs1.png";
-	public static final String ARCS_FG		= "shattered/arcs2.png";
-	public static final String DASHBOARD	= "shattered/dashboard.png";
-	
+public class Assets extends DefaultAssets {
+
 	public static final String BANNERS	= "shattered/banners.png";
-	public static final String BADGES	= "shattered/badges.png";
-	public static final String AMULET	= "shattered/amulet.png";
-	
-	public static final String CHROME	= "shattered/chrome.png";
-	public static final String ICONS	= "shattered/icons.png";
-	public static final String STATUS	= "shattered/status_pane.png";
-	public static final String HP_BAR	= "shattered/hp_bar.png";
-	public static final String XP_BAR	= "shattered/exp_bar.png";
-	public static final String TOOLBAR	= "shattered/toolbar.png";
-	
-	public static final String WARRIOR	= "shattered/warrior.png";
-	public static final String MAGE		= "shattered/mage.png";
-	public static final String ROGUE	= "shattered/rogue.png";
-	public static final String HUNTRESS	= "shattered/ranger.png";
-	public static final String AVATARS	= "shattered/avatars.png";
-	
-	public static final String SURFACE	= "shattered/surface.png";
-	
-	public static final String FIREBALL		= "shattered/fireball.png";
-	public static final String SPECKS		= "shattered/specks.png";
-	public static final String EFFECTS		= "shattered/effects.png";
-	
-	public static final String RAT		= "shattered/rat.png";
-	public static final String GNOLL	= "shattered/gnoll.png";
-	public static final String CRAB		= "shattered/crab.png";
-	public static final String GOO		= "shattered/goo.png";
-	public static final String SWARM	= "shattered/swarm.png";
-	public static final String SKELETON	= "shattered/skeleton.png";
-	public static final String SHAMAN	= "shattered/shaman.png";
-	public static final String THIEF	= "shattered/thief.png";
-	public static final String TENGU	= "shattered/tengu.png";
-	public static final String SHEEP	= "shattered/sheep.png";
-	public static final String KEEPER	= "shattered/shopkeeper.png";
-	public static final String BAT		= "shattered/bat.png";
 	public static final String BRUTE	= "shattered/brute.png";
-	public static final String SPINNER	= "shattered/spinner.png";
-	public static final String DM300	= "shattered/dm300.png";
-	public static final String WRAITH	= "shattered/wraith.png";
-	public static final String ELEMENTAL= "shattered/elemental.png";
-	public static final String MONK		= "shattered/monk.png";
-	public static final String WARLOCK	= "shattered/warlock.png";
-	public static final String GOLEM	= "shattered/golem.png";
-	public static final String UNDEAD	= "shattered/undead.png";
-	public static final String KING		= "shattered/king.png";
-	public static final String STATUE	= "shattered/statue.png";
-	public static final String PIRANHA	= "shattered/piranha.png";
-	public static final String EYE		= "shattered/eye.png";
-	public static final String SUCCUBUS	= "shattered/succubus.png";
-	public static final String SCORPIO	= "shattered/scorpio.png";
-	public static final String ROTTING	= "shattered/rotting_fist.png";
-	public static final String BURNING	= "shattered/burning_fist.png";
-	public static final String YOG		= "shattered/yog.png";
-	public static final String LARVA	= "shattered/larva.png";
-	public static final String GHOST	= "shattered/ghost.png";
-	public static final String MAKER	= "shattered/wandmaker.png";
-	public static final String TROLL	= "shattered/blacksmith.png";
-	public static final String IMP		= "shattered/demon.png";
-	public static final String RATKING	= "shattered/ratking.png";
-	
-	public static final String ITEMS	= "shattered/items.png";
-	public static final String PLANTS	= "shattered/plants.png";
-	
-	public static final String TILES_SEWERS	= "shattered/tiles0.png";
-	public static final String TILES_PRISON	= "shattered/tiles1.png";
-	public static final String TILES_CAVES	= "shattered/tiles2.png";
-	public static final String TILES_CITY	= "shattered/tiles3.png";
-	public static final String TILES_HALLS	= "shattered/tiles4.png";
-	
-	public static final String WATER_SEWERS	= "shattered/water0.png";
-	public static final String WATER_PRISON	= "shattered/water1.png";
-	public static final String WATER_CAVES	= "shattered/water2.png";
-	public static final String WATER_CITY	= "shattered/water3.png";
-	public static final String WATER_HALLS	= "shattered/water4.png";
-	
 	public static final String BUFFS_SMALL	= "shattered/buffs.png";
+	public static final String CRAB		= "shattered/crab.png";
+	public static final String FIREBALL		= "shattered/fireball.png";
+	
+	public static final String GNOLL	= "shattered/gnoll.png";
+	public static final String GOO		= "shattered/goo.png";
+	public static final String ICONS	= "shattered/icons.png";
+	public static final String ITEMS	= "shattered/items.png";
 	public static final String BUFFS_LARGE	= "shattered/large_buffs.png";
-	public static final String SPELL_ICONS	= "shattered/spell_icons.png";
 	
-	public static final String FONTS1X	= "shattered/font1x.png";
-	public static final String FONTS15X	= "shattered/font15x.png";
-	public static final String FONTS2X	= "shattered/font2x.png";
-	public static final String FONTS25X	= "shattered/font25x.png";
-	public static final String FONTS3X	= "shattered/font3x.png";
-	
-	public static final String THEME	= "shattered/theme.mp3";
-	public static final String TUNE		= "shattered/game.mp3";
-	public static final String HAPPY	= "shattered/surface.mp3";
-	
-	public static final String SND_CLICK	= "shattered/snd_click.mp3";
-	public static final String SND_BADGE	= "shattered/snd_badge.mp3";
-	public static final String SND_GOLD		= "shattered/snd_gold.mp3";
-	
-	public static final String SND_OPEN		= "shattered/snd_door_open.mp3";
-	public static final String SND_UNLOCK	= "shattered/snd_unlock.mp3";
-	public static final String SND_ITEM		= "shattered/snd_item.mp3";
-	public static final String SND_DEWDROP	= "shattered/snd_dewdrop.mp3";
-	public static final String SND_HIT		= "shattered/snd_hit.mp3";
-	public static final String SND_MISS		= "shattered/snd_miss.mp3";
-	public static final String SND_STEP		= "shattered/snd_step.mp3";
-	public static final String SND_WATER	= "shattered/snd_water.mp3";
-	public static final String SND_DESCEND	= "shattered/snd_descend.mp3";
-	public static final String SND_EAT		= "shattered/snd_eat.mp3";
-	public static final String SND_READ		= "shattered/snd_read.mp3";
-	public static final String SND_LULLABY	= "shattered/snd_lullaby.mp3";
-	public static final String SND_DRINK	= "shattered/snd_drink.mp3";
-	public static final String SND_SHATTER	= "shattered/snd_shatter.mp3";
-	public static final String SND_ZAP		= "shattered/snd_zap.mp3";
-	public static final String SND_LIGHTNING= "shattered/snd_lightning.mp3";
-	public static final String SND_LEVELUP	= "shattered/snd_levelup.mp3";
-	public static final String SND_DEATH	= "shattered/snd_death.mp3";
-	public static final String SND_CHALLENGE= "shattered/snd_challenge.mp3";
-	public static final String SND_CURSED	= "shattered/snd_cursed.mp3";
-	public static final String SND_TRAP		= "shattered/snd_trap.mp3";
-	public static final String SND_EVOKE	= "shattered/snd_evoke.mp3";
-	public static final String SND_TOMB		= "shattered/snd_tomb.mp3";
-	public static final String SND_ALERT	= "shattered/snd_alert.mp3";
-	public static final String SND_MELD		= "shattered/snd_meld.mp3";
-	public static final String SND_BOSS		= "shattered/snd_boss.mp3";
-	public static final String SND_BLAST	= "shattered/snd_blast.mp3";
-	public static final String SND_PLANT	= "shattered/snd_plant.mp3";
-	public static final String SND_RAY		= "shattered/snd_ray.mp3";
-	public static final String SND_BEACON	= "shattered/snd_beacon.mp3";
-	public static final String SND_TELEPORT	= "shattered/snd_teleport.mp3";
-	public static final String SND_CHARMS	= "shattered/snd_charms.mp3";
-	public static final String SND_MASTERY	= "shattered/snd_mastery.mp3";
-	public static final String SND_PUFF		= "shattered/snd_puff.mp3";
-	public static final String SND_ROCKS	= "shattered/snd_rocks.mp3";
-	public static final String SND_BURNING	= "shattered/snd_burning.mp3";
-	public static final String SND_FALLING	= "shattered/snd_falling.mp3";
-	public static final String SND_GHOST	= "shattered/snd_ghost.mp3";
-	public static final String SND_SECRET	= "shattered/snd_secret.mp3";
-	public static final String SND_BONES	= "shattered/snd_bones.mp3"; 
+	public static final String PLANTS	= "shattered/plants.png";
+	public static final String RAT		= "shattered/rat.png";
 }
